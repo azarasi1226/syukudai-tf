a0377c68edd7c35b1a3d98d988f88a0f59044a5c
ando_kazuki <azarasikazuki@gmail.com>
2024-06-02 19:34:59 +0900

    save

--- modules/network/management.tf
+++ modules/network/management.tf
@@ -1,6 +1,5 @@
 resource "aws_subnet" "management" {
     vpc_id = aws_vpc.this.id
-
     cidr_block = var.management_cidr
     availability_zone = data.aws_availability_zones.available.names[0]
     map_public_ip_on_launch = true
